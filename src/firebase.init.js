14c357588b3e80c0e1fb8f00a6d1b29d6e07dae1
Rijonahmed <rijon036@gmail.com>
2022-05-06 20:51:55 +0600

    env variable set

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBDXhxWn19C6fwkT_131YogVc1bZv6gS_U",
-  authDomain: "moto-dealer.firebaseapp.com",
-  projectId: "moto-dealer",
-  storageBucket: "moto-dealer.appspot.com",
-  messagingSenderId: "738629083874",
-  appId: "1:738629083874:web:82dd65c624cd525b6a34bb"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
